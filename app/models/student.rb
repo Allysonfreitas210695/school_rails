b2ac45c380b525c648a2e1a0f23afa14f030dcfb
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-02-28 15:33:44 -0300

    validando os atributos no models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ApplicationRecord
   belongs_to :teacher
+  validates :name, :registration, :teacher,  presence: true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,4 @@
 class Teacher < ApplicationRecord
   has_many :students
+  validates :name, :registration, :department, :discipline, presence: true
 end
